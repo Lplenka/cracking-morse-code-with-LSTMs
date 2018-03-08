3f39579092e252bc7958025435d9361457a3054d
lplenka <lalu.lenka@aptusdatalabs.com>
2018-03-08 17:13:39 +0530

    morse_encode script

---
+++ morse_encode.py
@@ -0,0 +1,17 @@
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+# construct the Morse dictionary
+alphabet = " ".join("abcdefghijklmnopqrstuvwxyz").split()
+values = ['.-', '-...', '-.-.', '-..', '.', '..-.', '--.', '....', '..', '.---', '-.-', '.-..', '--', '-.', '---', '.--.', '--.-','.-.', '...', ' - ', '..-', '...-', '.--', ' - ..-', ' - .--', ' - -..']
+morse_dict = dict(zip(alphabet, values))
+
+def morse_ecode(word):
+    letters = " ".join(word).split()
+    morse_code = list(map(lambda x : morse_dict[x],letters))
+    morse_code = "*".join(morse_code)
+
+    return morse_code
+
+#print(morse_ecode("abc"))
