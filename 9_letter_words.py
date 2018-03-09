3d6297715b58fc52f5eb9d0c0855cb173996c5f8
lplenka <lalu.lenka@aptusdatalabs.com>
2018-03-09 10:51:18 +0530

    Bug fix

--- 9_letter_words.py
+++ 9_letter_words.py
@@ -1,9 +1,10 @@
-docu = open("word_list.txt","r")
-new = open("word_list9.txt","w")
+import pickle
+
+new = open("word_list.txt","r")
 v = []
-for line in docu:
+for line in new:
     if len(line) == 9:
        v.append(line)
-
+print(len(v))
 with open('word_list9', 'wb') as fp:
     pickle.dump(v, fp)
